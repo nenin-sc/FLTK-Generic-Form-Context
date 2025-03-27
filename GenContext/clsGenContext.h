2a888618325e8cc009e9a056ea72ddd80f7d2b0f
nenin-sc <65463402+nenin-sc@users.noreply.github.com>
2025-03-27 09:49:47 +0000

    Update clsGenContext.h

--- GenContext/clsGenContext.h
+++ GenContext/clsGenContext.h
@@ -6,6 +6,24 @@
 #include <FL/Fl_Double_Window.H>
 /*
 Generic context class for Fl_Window
+
+Generic context class for Fl_Window
+Copyright (C) 20??-2023  Y.(E.N.)Naumovich
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public License
+   as published by the Free Software Foundation; either version 2.1 of
+   the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, see
+   <http://www.gnu.org/licenses/>.
+
 */
 #if defined(WIN32) || defined(_WIN32)
   // source:  https://stackoverflow.com/questions/31050113/how-to-extract-the-source-filename-without-path-and-suffix-at-compile-time
